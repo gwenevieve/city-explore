6ef4e04854ae19e69402d58e60e01f8cf45201c8
marie <32374789+gwenevieve@users.noreply.github.com>
2021-12-05 01:11:27 -0800

    Added background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import './App.scss';
 
+import './App.scss';
 import styled from 'styled-components';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
 import { Container, Card, Typography } from '@mui/material';
 
 import SearchBar from './components/Search';
-import Weather from './components/Weather';
+import Weather from './components/weather/Weather';
 import Map from './components/Map';
 
 import { Coordinates } from './models/coordinates';
 
+import background from './images/background.png';
+
 const App = (): JSX.Element => {
     const [location, setLocation] = React.useState<Coordinates | undefined>(undefined);
     const [locationName, setLocationName] = React.useState<string | undefined>();
+
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
     return (
         <AppContainer>
+            {`theme.breakpoints.up('sm') matches: ${matches}`}
             <SearchBar setLocation={setLocation} setLocationName={setLocationName} />
             {location && (
                 <>
                     <Container maxWidth="md">
                         <CityCard>
                             <Banner />
-                            <Title variant="h1">{locationName}</Title>
-                            <Weather location={location} />
+                            <CityDetails>
+                                <Title variant="h1">{locationName}</Title>
+                                <Weather location={location} />
+                            </CityDetails>
                             <Map location={location} />
                         </CityCard>
                     </Container>
@@ -32,9 +44,19 @@ const App = (): JSX.Element => {
     );
 };
 
-const AppContainer = styled.main``;
+const AppContainer = styled.main`
+    height: 100vh;
+    background-image: url(${background});
+    background-color: #f9f2eb;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
 
 const CityCard = styled(Card)``;
+
+const CityDetails = styled.div`
+    padding: 20px;
+`;
 
 const MiniCard = styled(Card)``;
 
